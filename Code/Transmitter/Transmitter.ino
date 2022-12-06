79e3189c44ec60b384313a92ed81f3e9d737543c
Gabriele Mangione <i00204438@flowtec.endress.com>
2022-12-06 10:28:14 +0100

    test update

--- Code/Transmitter/Transmitter.ino
+++ Code/Transmitter/Transmitter.ino
@@ -5,6 +5,7 @@
 #include <SPI.h>
 
 #include "generalDefines.h"
+#include "RFM69registers.h"
 
 // Display defines
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
@@ -22,7 +23,7 @@
 RFM69 Radio(RFM69_CS, RFM69_INT);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-uint8_t sensorDistances[NUMBER_OF_SENSORS] = {0};
+uint8_t sensorDistances[NUMBER_OF_SENSORS] = { 0 };
 uint32_t lastRadioCommunication = 0;
 
 void setup() {
@@ -39,6 +40,7 @@ void setup() {
     }
     Radio.setHighPower();
     Radio.setIsrCallback(myCallback);  // set myCallback() function to call in the interrupt function of the radio library
+    Radio.setMode(RF69_MODE_RX);
 
     // display setup
     while (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -51,13 +53,26 @@ void setup() {
 }
 
 void loop() {
+    Serial.println("Heyo allocation failed");
     // read Joystick inputs
     uint8_t outputMotorSpeed = map(analogRead(A0), 249, 772, 0, 200);
     uint8_t outputSteerAngle = map(analogRead(A1), 248, 775, 0, 90);
-    uint8_t buf[2] = {outputMotorSpeed, outputSteerAngle};
-
+    uint8_t buf[2] = { outputMotorSpeed, outputSteerAngle };
+    Serial.println("1");
+    Radio.send(3,buf,2);
+    /*
+    Radio.writeReg(REG_PACKETCONFIG2, (Radio.readReg(REG_PACKETCONFIG2) & 0xFB) | RF_PACKET2_RXRESTART);  // avoid RX deadlocks
     // send values of speed and angle to the boat
-    Radio.send(3, buf, 2);
+    if (Radio.canSend()) {
+        Serial.println("5");
+
+
+        Radio.sendFrame(3, buf, 2);
+        Serial.println("6");
+        Radio.setMode(RF69_MODE_RX);
+        Serial.println("2");
+    }
+    */
 
     // print sent values
     display.clearDisplay();
@@ -70,6 +85,7 @@ void loop() {
     display.print(outputSteerAngle - 45);
     display.setCursor(120, 11);
     display.println("o");
+    Serial.println("3");
 
     // display distances of ultrasound sensors
     display.setCursor(0, 32);
@@ -77,7 +93,7 @@ void loop() {
         display.print("Distance ");
         display.print(i);
         display.print(":  ");
-        display.println(sensorDistances[i-1]);
+        display.println(sensorDistances[i - 1]);
     }
 
     //display time since last message from boat
@@ -88,8 +104,11 @@ void loop() {
 }
 
 void myCallback() {
-    for(uint8_t i = 0; i < NUMBER_OF_SENSORS; i++){
+    Serial.println("weyo");
+    Radio.setMode(RF69_MODE_STANDBY);
+    for (uint8_t i = 0; i < NUMBER_OF_SENSORS; i++) {
         sensorDistances[i] = Radio.DATA[i];
     }
     lastRadioCommunication = millis();
+    Radio.setMode(RF69_MODE_RX);
 }
